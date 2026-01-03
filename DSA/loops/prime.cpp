5cc4d5877da1e4848b11e407c62f91b4ab78d239
hamstersyntax <hamster.syntax@gmail.com>
2026-01-03 10:03:22 +0500

    removing all commits

--- DSA/loops/prime.cpp
+++ DSA/loops/prime.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
-// Program to Check whether  is Number prime or not
 int main()
 {
     bool isPrime(true);
     int n;
-    // Getting Input
+
     cout << "Enter A Number to Check ";
     cin >> n;
     for (int i = 2; i * i <= n; i++)
@@ -16,7 +15,6 @@ int main()
             break;
         }
     }
-    // Print Output
     isPrime ? cout << n << " Is prime " : cout << "It's not a prime number";
     return 0;
 }
