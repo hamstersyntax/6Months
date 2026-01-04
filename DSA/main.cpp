7ed81a552daea06c2c67d63b815b4dc091ea686c
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 12:51:44 +0500

    4-Jan

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -1,8 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        for (int space = 0; space < n - i - 1; space++)
+        {
+            cout << "*";
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout << i+1;
+        }
+        cout << endl;
+    }
     return 0;
 }
