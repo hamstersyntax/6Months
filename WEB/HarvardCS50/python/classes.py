215ddcafda0bb295f4f338b47a33ca346f32dcfe
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 17:40:59 +0500

    commit at Sunday - 17:40

--- WEB/HarvardCS50/python/classes.py
+++ WEB/HarvardCS50/python/classes.py
@@ -6,6 +6,8 @@ class Student():
        self.cAge  =  age;
     def sayName(self):
         print(f"Student : {self.cName} Age : {self.cAge}")
+    def changeName(self):
+        
 
 c =Student("Hamster",20);
 c.sayName()
