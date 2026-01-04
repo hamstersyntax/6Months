ec993cd25936d0fe77de8884afbd8bc25ea474be
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 20:27:58 +0500

    Day Django / Sunday - 20:27

--- WEB/HarvardCS50/Django/mysite/hello/views.py
+++ WEB/HarvardCS50/Django/mysite/hello/views.py
@@ -6,4 +6,4 @@ def index(request):
 def hamster(request):
     return HttpResponse("Hamster Syntax")
 def sayName(request,name):
-    return HttpResponse(f"Hello {name.cap}")
+    return HttpResponse(f"Hello {name.captilize()}")
