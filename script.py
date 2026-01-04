2906be3a8fdad7f311339afc9dd3943d90c9fa33
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 17:40:00 +0500

    commit at Sunday - 17:40

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ while True:
     if status:
         timestamp = datetime.now().strftime("%A - %H:%M")
         commit_message = f"commit at {timestamp}"
-        print(f"✅ {} ")
+        print(f"✅ {commit_message} ")
         os.system("git add .")
         os.system(f'git commit -m "{commit_message}"')
         os.system("git push")
