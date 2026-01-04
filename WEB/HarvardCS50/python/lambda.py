df6b3a9944f9b9c39c82fde38f1dca772a532d5d
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 18:03:22 +0500

    commit at Sunday - 18:03

--- WEB/HarvardCS50/python/lambda.py
+++ WEB/HarvardCS50/python/lambda.py
@@ -1,8 +1,8 @@
 import fnc ; fnc.clearScreen();
 fruits = [
-    {"name","mango"},
-    {"name","banana"},
-    {"name","apple"}
+    {"name":"mango"},
+    {"name":"banana"},
+    {"name":"apple"}
 ]
 
 fruits.sort(key=lambda fruit:fruit['name'])
