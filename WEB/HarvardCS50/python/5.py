60a090ef014e86e183a8918cd8b3aecf160e1f02
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 14:45:46 +0500

    commit at 2026 - 14:45

--- WEB/HarvardCS50/python/5.py
+++ WEB/HarvardCS50/python/5.py
@@ -3,11 +3,19 @@ os.system("cls");
 name = "Hamster"
 for char in name:
     print(char)
-
+# H
+# a
+# m
+# s
+# t
+# e
+# r
 ls = ["apple","mango","banana"]
 for e in ls:
     print(e)
-
+# apple
+# mango
+# banana
 st = {1,2,3,4,5,5,6,7,8,9,2}
 for i in st:
     if i%2==0:
