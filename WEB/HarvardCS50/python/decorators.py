6b4f5e1ab6f90c7eb3a35e9b40f16e65e5c8ffcf
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 17:49:46 +0500

    commit at Sunday - 17:49

--- WEB/HarvardCS50/python/decorators.py
+++ WEB/HarvardCS50/python/decorators.py
@@ -1,9 +1,10 @@
 import fnc;fnc.clearScreen();
-def sayName(fnc):
+def getName(fnc):
     def wrapper():
         print("Before Running fnc...")
         fnc()
         print("Done with fnc....")
         return wrapper;
     
-@
+@sayName
+def 
