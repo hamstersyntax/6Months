c0b1a6af9063a1967edd24d6b538933eb847ab87
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 20:18:51 +0500

    Day Django / Sunday - 20:18

--- WEB/HarvardCS50/Django/mysite/hello/urls.py
+++ WEB/HarvardCS50/Django/mysite/hello/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("hamster", views.hamster, name="hamster"),
-    path("<str:name>",views,sayName,name="")
+    path("<str:name>",views,sayName,name="sayName")
 ]
