a1dc8d6cb1e1227bf3ae4b2b65c8fe8b49957a4f
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 13:33:13 +0500

    auto commit at 2026-01-04 13:33:13

---
+++ WEB/HarvardCS50/python/2.py
@@ -0,0 +1,5 @@
+import 
+name =  input("Name : "); # getting input
+age = int(input("Age : ")); # type conversion
+print(f"Your Name is {name}");
+print(f"Age is {age}");
---
+++ script.py
@@ -0,0 +1,16 @@
+import os
+import time
+from datetime import datetime
+
+while True:
+    status = os.popen("git status --porcelain").read().strip()
+
+    if status:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        commit_message = f"auto commit at {timestamp}"
+
+        os.system("git add .")
+        os.system(f'git commit -m "{commit_message}"')
+        os.system("git push")
+
+    time.sleep(60)
