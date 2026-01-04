5b94d0b964214640c304153693b3f2b071cda4e0
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 13:51:22 +0500

    Some New Updates at 2026-01-04 13:51:21

--- WEB/HarvardCS50/python/3.py
+++ WEB/HarvardCS50/python/3.py
@@ -1,9 +1,10 @@
 import os;
 os.system("cls");
 num  = int(input("Enter Any Number : "));
+
 if num > 0:
     print("Positive");
-else if num < 0:
+elseif num < 0:
     print("Negative");
 else :
     print("is zero");
