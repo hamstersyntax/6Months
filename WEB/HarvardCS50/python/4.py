38f5d570cc3e8b665c1e4383f0301c6defdc48fe
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 14:22:50 +0500

    commit at 2026 - 14:22

--- WEB/HarvardCS50/python/4.py
+++ WEB/HarvardCS50/python/4.py
@@ -7,7 +7,7 @@ print(fruits)
 fruits.sort()
 print(fruits)
 # sets of fruits
-fruitset = {"apple","mango","banana" , "apple"}
+fruitset = {"apple","mango","banana" , "apple" , "banana"}
 
 print(fruitset)
 
