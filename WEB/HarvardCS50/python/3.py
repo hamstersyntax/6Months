23130ff7d269d1c2be2563e0010d3090072e1ca5
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 13:56:08 +0500

    Some New Updates at 2026-01-04 13:56:07

--- WEB/HarvardCS50/python/3.py
+++ WEB/HarvardCS50/python/3.py
@@ -2,15 +2,15 @@ import os;
 os.system("cls");
 num  = int(input("Enter Any Number : "));
 
-# if num > 0:
-#     print("Positive");
-# elif num < 0:
-#     print("Negative");
-# else :
-#     print("is zero");
+if num > 0:
+    print("Positive");
+elif num < 0:
+    print("Negative");
+else :
+    print("is zero");
 
-match num:
-    case x if x >= 0:
-        print("Positive");
-    case _:
-        print("Negative");
+# match num:
+#     case x if x >= 0:
+#         print("Positive");
+#     case _:
+#         print("Negative");
