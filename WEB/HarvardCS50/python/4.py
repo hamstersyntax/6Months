528ceafcfa5a3b95189ead2e99ec888fc45decee
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 14:23:05 +0500

    commit at 2026 - 14:23

--- WEB/HarvardCS50/python/4.py
+++ WEB/HarvardCS50/python/4.py
@@ -3,9 +3,9 @@ os.system("cls");
 # List of fruits
 fruits = ["apple","mango"];
 fruits.append("apple")
-print(fruits)
-fruits.sort()
-print(fruits)
+# print(fruits)
+# fruits.sort()
+# print(fruits)
 # sets of fruits
 fruitset = {"apple","mango","banana" , "apple" , "banana"}
 
