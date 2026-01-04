a76b3c95fd52031e8b672bf58a41cf2cc52fd752
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 20:22:38 +0500

    Day Django / Sunday - 20:22

--- WEB/HarvardCS50/Django/mysite/hello/views.py
+++ WEB/HarvardCS50/Django/mysite/hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Syntax!!")
+    return render
 def hamster(request):
     return HttpResponse("Hamster Syntax")
 def sayName(request,name):
