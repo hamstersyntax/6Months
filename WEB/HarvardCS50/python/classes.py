f97ed6ebc65df4d4aa0cf0b6cd13fb6d606717fe
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 17:26:48 +0500

    commit at 2026 - 17:26

--- WEB/HarvardCS50/python/classes.py
+++ WEB/HarvardCS50/python/classes.py
@@ -2,8 +2,8 @@ import fnc; fnc.clearScreen();
 
 class Student():
     def __init__(self,name,age):
-       cName = name;
-       cAge  =  age;
+       self.cName = name;
+       self.cAge  =  age;
     def sayName(self):
         print(f"Student : {self.cName} Age : {self.cAge}")
 
