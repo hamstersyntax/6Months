9a282488afa50683b7f6387f5a1b1e04fee45ffe
hamstersyntax <hamster.syntax@gmail.com>
2026-01-04 14:43:04 +0500

    commit at 2026 - 14:43

--- WEB/HarvardCS50/python/5.py
+++ WEB/HarvardCS50/python/5.py
@@ -4,9 +4,9 @@ os.system("cls");
 # for char in name:
 #     print(char)
 
-# ls = ["apple","mango","banana"]
-# for e in ls:
-#     print(e)
+ls = ["apple","mango","banana"]
+for e in ls:
+    print(e)
 
 st = {1,2,3,4,5,5,6,7,8,9,2}
 for i in st:
