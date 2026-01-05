642a9fa377068bffb4629e4e757a8407d61c52c8
hamstersyntax <hamster.syntax@gmail.com>
2026-01-05 17:00:51 +0500

    Day DSA Pattern / Monday - 17:00

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -1,30 +1,8 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    int n = 4;
-    int totalRow = 2 * n - 1;
-    int mid = n - 1;
-    for (int row = 0; row < totalRow; row++)
-    {
-        int dist = abs(row - mid);
-        for (int l = 0; l < dist; l++)
-        {
-            cout << " ";
-        }
-        cout << "*";
-
-        int innerSpaces = 2 * (n - dist - 1) - 1;
-        if (innerSpaces >= 0)
-        {
-            for (int r = 0; r < innerSpaces; r++)
-            {
-                cout << " ";
-            }
-            cout << "*";
-        }
-        cout << endl;
-    }
+    int n
     return 0;
 }
