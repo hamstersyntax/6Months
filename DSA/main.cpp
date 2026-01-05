f62d713611d904cb348535534b292d56a2aaa64c
hamstersyntax <hamster.syntax@gmail.com>
2026-01-05 15:04:51 +0500

    Day DSA Pattern / Monday - 15:04

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -13,7 +13,7 @@ int main()
         cout << "*";
         if (i != 0)
         {
-            for (int spaceR = 0; spaceR < ((2 * n) - 1); spaceR++)
+            for (int spaceR = 0; spaceR <= ((2 * n) - 1); spaceR++)
             {
                 cout << " ";
                 spaceR == ((2 * n) - 1) && cout << "*";
