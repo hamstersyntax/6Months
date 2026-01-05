0678d7844b69fd7e155704fd8c73a2323eaa9054
hamstersyntax <hamster.syntax@gmail.com>
2026-01-05 16:51:37 +0500

    Day DSA Pattern / Monday - 16:51

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -5,10 +5,10 @@ int main()
 {
     int n=4;
     int totalRow = 2*n-1;
-
+    int mid = 
     for (int row = 0; row < totalRow; row++)
     {
-        int dist = abs()
+        int dist = abs(row-n);
     }
     
     return 0;
