3ed5fc18db78b0fdc658149b339d679111f80b1d
hamstersyntax <hamster.syntax@gmail.com>
2026-01-05 14:55:01 +0500

    Day DSA Pattern / Monday - 14:55

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -6,7 +6,7 @@ int main()
     int n = 4;
     for (int i = 0; i < n; i++)
     {
-        for (int spaceUL = n; spaceUL > 0; spaceUL--)
+        for (int spaceUL = n - i; spaceUL > 0; spaceUL--)
         {
             cout << "#";
         }
