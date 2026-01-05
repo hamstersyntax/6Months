c618ab63ecfd450a8d54bca88918a0085dc97163
hamstersyntax <hamster.syntax@gmail.com>
2026-01-05 15:07:55 +0500

    Day DSA Pattern / Monday - 15:07

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -19,7 +19,25 @@ int main()
             }
             cout << "*";
         }
+        cout << endl;
+    }
 
+    // Lower Half
+     for (int i = 0; i < n; i++)
+    {
+        for (int spaceL = n - i - 1; spaceL > 0; spaceL--)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        if (i > 0)
+        {
+            for (int spaceR = 0; spaceR < ((2 * i) - 1); spaceR++)
+            {
+                cout << " ";
+            }
+            cout << "*";
+        }
         cout << endl;
     }
 
