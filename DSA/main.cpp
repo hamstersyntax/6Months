4b2fdb5bbae68ca175b6011b7f106d0377d66ad3
hamstersyntax <hamster.syntax@gmail.com>
2026-01-05 15:35:37 +0500

    Day DSA Pattern / Monday - 15:35

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -24,7 +24,7 @@ int main()
         cout << endl;
     }
     // Lower Half
-    for (int i = N ; i > 0; i--)
+    for (int i = N - 1; i > 0; i--)
     {
         for (int l = 0; l < i; l++)
         {
