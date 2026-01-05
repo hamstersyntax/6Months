d92987feb7542a1c21628be9ba2ab01571fb50be
hamstersyntax <hamster.syntax@gmail.com>
2026-01-05 17:10:08 +0500

    Day DSA Pattern / Monday - 17:10

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -7,17 +7,14 @@ int main()
     for (int i = 0; i < n; i++)
     {
         int spaces = n - i - 1;
-        int star = spaces - (i + 1);
         for (int j = 0; j < spaces; j++)
         {
             cout << "#";
         }
         for (int star = 0; star < n; star++)
         {
-            /* code */
+            cout << "*";
         }
-        
-        
         cout << endl;
     }
     return 0;
