a640da0264d5f3b3e9e56eb810078ee9a5a4167f
hamstersyntax <hamster.syntax@gmail.com>
2026-01-05 15:47:28 +0500

    Day DSA Pattern / Monday - 15:47

--- DSA/loops/pattern14.cpp
+++ DSA/loops/pattern14.cpp
@@ -9,7 +9,7 @@ int main()
     for (int row = 1; row <= totalRows; row++)
     {
         int stars = (row <= N) ? row : (2 * N - row);
-        int spaces = 2 * (N - stars);
+        int spaces = (2 * N - stars);
 
         // Left stars
         for (int i = 0; i < stars; i++)
