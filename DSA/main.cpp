3edc95c3e72a9c6d272d3b3e1ee0e350f524902b
hamstersyntax <hamster.syntax@gmail.com>
2026-01-07 16:46:26 +0500

    Day 2 DSA Pattern / Wednesday - 16:46

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -11,10 +11,12 @@ int main()
 
         if (i != 0 || i != n - 1)
         {
+            cout << "*";
             for (int i = 0; i < hollow; i++)
             {
                 cout << " ";
             }
+            cout << "*";
         }
     }
 
