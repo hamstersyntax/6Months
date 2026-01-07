18f5cafcaa3cdcebdcfd17b1eb92c4ac7c201bf9
hamstersyntax <hamster.syntax@gmail.com>
2026-01-07 15:37:24 +0500

    Day 2 DSA Pattern / Wednesday - 15:37

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -4,15 +4,22 @@ using namespace std;
 int main()
 {
     int n = 5;
-    int totalRow = 2 * n - 1;
-    int mid = 2 * n - 1;
-    for (int row = 0; row < totalRow; row++)
+    int totalRows = 2 * n - 1;
+    int mid = n - 1;
+
+    for (int row = 0; row < totalRows; row++)
     {
-        int stars = row <= totalRow ? 2 * row + 1 : stars - 2;
-        for (int i = 0; i < stars; i++)
-        {
+        int dist = abs(row - mid);
+
+        int spaces = dist;
+        int stars = 2 * (n - dist) - 1;
+
+        for (int s = 0; s < spaces; s++)
+            cout << " ";
+
+        for (int s = 0; s < stars; s++)
             cout << "*";
-        }
+
         cout << endl;
     }
 
