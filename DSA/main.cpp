7da7c0b15a62a6622615981b5eda235d2326b20a
hamstersyntax <hamster.syntax@gmail.com>
2026-01-07 16:44:06 +0500

    Day 2 DSA Pattern / Wednesday - 16:44

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -6,17 +6,16 @@ int main()
     int n = 5;
     for (int i = 0; i < n; i++)
     {
-        int hollow = n - 2;
+        cout 
+        // int hollow = n - 2;
         
-        if (i == 0 || i == n - 1)
-        {
+        // if (i == 0 || i == n - 1)
+        // {
            
             
-        }
-        else
-        {
-            
-        }
+        // }
+       
+       
 
         cout << endl;
     }
