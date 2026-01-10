f0ebbffbe33f09b4bd00268264c58a42906b104a
hamstersyntax <hamster.syntax@gmail.com>
2026-01-10 13:37:54 +0500

    Day 3 DSA / Saturday - 13:37

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
+int 
 int main()
 {
-    int n1 = pow(2, 1);
-    int n2 = pow(2, 0);
-    int n3 = pow(2, 1);
-    cout << n1 + n2 + n3;
+ 
     return 0;
 }
