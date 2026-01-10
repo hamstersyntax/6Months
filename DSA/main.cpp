7b78d71954d3d0115ab68ee0f77b83027b8719ef
hamstersyntax <hamster.syntax@gmail.com>
2026-01-10 11:12:39 +0500

    Day 3 DSA / Saturday - 11:12

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -1,29 +1,8 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    int n = 5;
-    int hollow = n - 2;
-    for (int i = 0; i < n; i++)
-    {
-
-        if (i == 0 || i == n - 1)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                cout << "*";
-            }
-            cout << endl;
-        }
-        cout << "*";
-        for (int i = 0; i < hollow; i++)
-        {
-            cout << " ";
-        }
-        cout << "*";
-        cout << endl;
-    }
-
+    
     return 0;
 }
