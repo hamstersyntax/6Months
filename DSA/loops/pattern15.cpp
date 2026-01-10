4a554a31f335510773933f6ae7db27dbcc21189a
hamstersyntax <hamster.syntax@gmail.com>
2026-01-10 11:24:08 +0500

    Day 3 DSA / Saturday - 11:24

--- DSA/loops/pattern15.cpp
+++ DSA/loops/pattern15.cpp
@@ -1,25 +1,32 @@
-int n = 4;
-int totalRows = 2 * n - 1;
-int mid = n - 1;
+#include <iostream>
+using namespace std;
 
-for (int row = 0; row < totalRows; row++)
+int main()
 {
-    int dist = abs(row - mid);
+    int n = 4;
+    int totalRows = 2 * n - 1;
+    int mid = n - 1;
 
-    for (int s = 0; s < dist; s++)
-        cout << " ";
+    for (int row = 0; row < totalRows; row++)
+    {
+        int dist = abs(row - mid);
 
-    for (int s = 0; s < 2 * (n - dist) - 1; s++)
-        cout << "*";
+        for (int s = 0; s < dist; s++)
+            cout << " ";
 
-    cout << endl;
+        for (int s = 0; s < 2 * (n - dist) - 1; s++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    // *******
+    //  *****
+    //   ***
+    //    *
+    //   ***
+    //  *****
+    // *******
+
+    return 0;
 }
-
-
-*******
- *****
-  ***
-   *
-  ***
- *****
-*******
