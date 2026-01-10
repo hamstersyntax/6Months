a9f88d13cb5b109ad57704cbd4f40ef4a4ab0bf2
hamstersyntax <hamster.syntax@gmail.com>
2026-01-10 13:31:21 +0500

    Day 3 DSA / Saturday - 13:31

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -1,8 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+string decToBinary(int num)
+{
+    string binary = "";
+    while (num > 0)
+    {
+        binary = to_string(num % 2) + binary;
+        num /= 2;
+    }
 
+    return binary;
+}
 int main()
 {
-    
+    for (int i = 0; i < 10; i++)
+    {
+        i != 0
+            ? cout << i << " - " << decToBinary(i) << endl
+            : cout << i << " - " << "0" << endl;
+    }
     return 0;
 }
