f6e6cad58a8f81b566c884c941d6dffa0be592ad
hamstersyntax <hamster.syntax@gmail.com>
2026-01-10 13:15:40 +0500

    Day 3 DSA / Saturday - 13:15

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -6,18 +6,19 @@ int power(int n, int b)
     int pow;
     for (int i = 0; i < b; i++)
     {
-        pow =  n *b;
+        pow = n * b;
     }
+    return pow;
 }
 int main()
 {
     int num = 5;
     int rem, decNum;
-    while (num > 0)
-    {
-        rem = num % 2;
-        decNum += 2 * 10;
-    }
+    // while (num > 0)
+    // {
+    //     rem = num % 2;
+    //     decNum += 2 * 10;
+    // }
 
     return 0;
 }
