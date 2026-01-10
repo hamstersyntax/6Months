a5030ecaa4ff17b29d6a83d0e0d85a96724a0d8c
hamstersyntax <hamster.syntax@gmail.com>
2026-01-10 13:28:09 +0500

    Day 3 DSA / Saturday - 13:28

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 string decToBinary(int num)
 {
     string binary = "";
-    while (num >= 0)
+    while (num > 0)
     {
         binary = to_string(num % 2) + binary;
         num /= 2;
