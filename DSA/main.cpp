a646c6c2a55fdeb839972fdead1c42e43074e2a1
hamstersyntax <hamster.syntax@gmail.com>
2026-01-10 13:39:29 +0500

    Day 3 DSA / Saturday - 13:39

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int reverse(int n ){
+int reverse(int n)
+{
 
+    int result = 0;
     while (n > 0)
     {
-    
     }
-    
 }
 int main()
 {
- 
+
     return 0;
 }
