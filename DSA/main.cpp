e45d62a3a7060dfb4d9bd6bfb8af85066d49554a
hamstersyntax <hamster.syntax@gmail.com>
2026-01-10 13:15:01 +0500

    Day 3 DSA / Saturday - 13:15

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -1,18 +1,22 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
-int power(int n , int b){
-    
+int power(int n, int b)
+{
+    for (int i = 0; i < b; i++)
+    {
+       re n *b;
+    }
 }
 int main()
 {
-    int num =5 ;
-    int rem,decNum;
+    int num = 5;
+    int rem, decNum;
     while (num > 0)
     {
-       rem = num%2;
-       decNum += 2*10;
+        rem = num % 2;
+        decNum += 2 * 10;
     }
-    
+
     return 0;
 }
