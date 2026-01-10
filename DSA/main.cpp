c5e5fb92b9741420b8ffbc94b9dadff1f27fc3d2
hamstersyntax <hamster.syntax@gmail.com>
2026-01-10 13:43:00 +0500

    Day 3 DSA / Saturday - 13:42

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -8,10 +8,10 @@ int reverse(int n)
     int rem;
     while (n > 0)
     {
-        result += pow;
-        
+        result += pow(2, n % 10);
         n /= 10;
     }
+    cout << result;
 }
 int main()
 {
