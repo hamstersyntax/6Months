54e43cb458f4a0f97f9b8be9cfb10d7352f4b00b
hamstersyntax <hamster.syntax@gmail.com>
2026-01-10 13:36:54 +0500

    Day 3 DSA / Saturday - 13:36

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main()
 {
-    int   n1 = cout << pow(2, 1);
-    cout << pow(2, 0);
-    cout << pow(2, 1);
+    int n1 = cout << pow(2, 1);
+    int n2 = cout << pow(2, 0);
+    int n3 = cout << pow(2, 1);
+    cout 
     return 0;
 }
