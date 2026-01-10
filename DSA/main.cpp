c4efad31ac47f237bf8812bb0974d4798730be7b
hamstersyntax <hamster.syntax@gmail.com>
2026-01-10 13:36:17 +0500

    Day 3 DSA / Saturday - 13:36

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
+#include <iostream>
 #include <cmath>
 using namespace std;
 
 int main()
 {
-  cout <<   pow(2,0);
+    cout << pow(2, 1);
+    cout << pow(2, 0);
+    cout << pow(2, 1);
     return 0;
 }
