1b21c5133ba73be9192641ccba5beb14ee6cc4a6
hamstersyntax <hamster.syntax@gmail.com>
2026-01-10 13:28:56 +0500

    Day 3 DSA / Saturday - 13:28

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -15,8 +15,9 @@ int main()
 {
     for (int i = 0; i < 10; i++)
     {
-       i != 0 ?
-       cout << i << " - " << decToBinary(i) << end
+       i != 0
+       ?cout << i << " - " << decToBinary(i) << end
+       :cout << "0"
     }
 
     return 0;
